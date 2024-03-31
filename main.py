1a9a9723a62c5cb10a99b20126354b24a86b4918
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2024-03-31 15:36:04 +0200

    done

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 
 
 # Set the input and output directories
-input_directory = "/Users/omidshojaeianzanjani/Desktop/Mega2"
-output_directory = "/Users/omidshojaeianzanjani/Desktop/Mega2/output_images"
+input_directory = "/Users/omidshojaeianzanjani/Desktop/Mega2/input_images"
+output_directory = "/Users/omidshojaeianzanjani/Desktop/Mega2/outputs"
 
 # 6.7” :  1290 x 2796px or 2796 x 1290px
 
@@ -15,23 +15,22 @@ output_directory = "/Users/omidshojaeianzanjani/Desktop/Mega2/output_images"
 
 # Screenshots dimensions should be: 2048 x 2732px or 2732 x 2048px
 # Screenshots dimensions should be: 2048 x 2732px or 2732 x 2048px
-target_sizes = {
-    (1290, 2796): "output_images_6.7 inch - 1290 x 2796",
-    (2796, 1290): "output_images_6.7 inch - 2 - 2796 x 1290",
-    (1242, 2688): "output_images_6.5 inch - 1242x2688",
-    (1284, 2778): "output_images_6.5 inch- 2 - 1284x2778",
-    (1242, 2208): "output_images_5.5 inch - 1242x2208",
-
-    (2048, 2732): "output_images_iPad 6 edition 12.9 inch - 2048 x 2732",
-    (2732, 2048): "output_images_iPad 6 edition 12.9 inch - 2732 x 2048",
-    (2048, 2732): "output_images_iPad 6 edition 12.9 inch - 2 edition 2048 x 2732",
-    (2732, 2048): "output_images_iPad 6 edition 12.9 inch - 2 edition 2732 x 2048"
+target_sizes = [
+    ((1290, 2796), "output_images_6.7 inch - 1290 x 2796"),
+    ((2796, 1290), "output_images_6.7 inch - 2 - 2796 x 1290"),
+    ((1242, 2688), "output_images_6.5 inch - 1242x2688"),
+    ((1284, 2778), "output_images_6.5 inch- 2 - 1284x2778"),
+    ((1242, 2208), "output_images_5.5 inch - 1242x2208"),
+    ((2048, 2732), "output_images_iPad 6 edition 12.9 inch - 2048 x 2732"),
+    ((2732, 2048), "output_images_iPad 6 edition 12.9 inch - 2732 x 2048"),
+    ((2048, 2732), "output_images_iPad 2 edition 12.9 inch - 2048 x 2732"),
+    ((2732, 2048), "output_images_iPad 2 edition 12.9 inch - 2732 x 2048"),
     # Add more target sizes and corresponding output directories as needed
-}
-
+]
 # Ensure the output directories exist
-for output_directory in target_sizes.values():
-    os.makedirs(output_directory, exist_ok=True)
+for _, output_dir in target_sizes:
+    folder_path = os.path.join(output_directory, output_dir)
+    os.makedirs(folder_path, exist_ok=True)
 
 # Loop through all images in the input directory
 for filename in os.listdir(input_directory):
@@ -40,8 +39,12 @@ for filename in os.listdir(input_directory):
         image_path = os.path.join(input_directory, filename)
         img = Image.open(image_path)
 
+        
         # Resize the image for each target size
-        for target_size, output_dir in target_sizes.items():
+        for itemInfo in target_sizes:
+            print(itemInfo[1])
+            target_size = itemInfo[0]
+            output_dir = os.path.join(output_directory, itemInfo[1])
             # Resize the image
             resized_img = img.resize(target_size, Image.ANTIALIAS)
 
