6e9680a408b4bad997092f779a39897b2806fcbf
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2024-03-31 14:41:05 +0200

    done

--- main.py
+++ main.py
@@ -12,12 +12,20 @@ output_directory = "/Users/omidshojaeianzanjani/Desktop/Mega2/output_images"
 
 # 5.5” : 1242x2208
 # Define the target sizes and corresponding output directories
+
+# Screenshots dimensions should be: 2048 x 2732px or 2732 x 2048px
+# Screenshots dimensions should be: 2048 x 2732px or 2732 x 2048px
 target_sizes = {
     (1290, 2796): "output_images_6.7 inch - 1290 x 2796",
     (2796, 1290): "output_images_6.7 inch - 2 - 2796 x 1290",
     (1242, 2688): "output_images_6.5 inch - 1242x2688",
     (1284, 2778): "output_images_6.5 inch- 2 - 1284x2778",
-    (1242, 2208): "output_images_5.5 inch - 1242x2208"
+    (1242, 2208): "output_images_5.5 inch - 1242x2208",
+
+    (2048, 2732): "output_images_iPad 6 edition 12.9 inch - 2048 x 2732",
+    (2732, 2048): "output_images_iPad 6 edition 12.9 inch - 2732 x 2048",
+    (2048, 2732): "output_images_iPad 6 edition 12.9 inch - 2 edition 2048 x 2732",
+    (2732, 2048): "output_images_iPad 6 edition 12.9 inch - 2 edition 2732 x 2048"
     # Add more target sizes and corresponding output directories as needed
 }
 
